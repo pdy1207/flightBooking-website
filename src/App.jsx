b703cd1c60791e697572f12917bf2f81a97da94e
pdy1207 <pdyme1207@gmail.com>
2023-07-23 16:01:05 +0900

    chore: 파일 변경 Travelers ++ Subscribe 다른 박스로서 구성

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Search from "./Components/Search/Search";
 import Support from "./Components/Support/Support";
 import Info from "./Components/Info/Info";
 import Lounge from "./Components/Lounge/Lounge";
-import Travelers from "./Components/Subscribers/Subscribe";
+import Travelers from "./Components/Travelers/Travelers";
 import Subscribe from "./Components/Subscribers/Subscribe";
 import Footer from "./Components/Footer/Footer";
 
